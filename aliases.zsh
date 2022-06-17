3ea363ddde9fc8c2a7db8039caae88ffdbfc8266
Mia Chen <mia.chen@jitbase.com>
2022-06-17 16:22:49 -0400

    Add gitsetupjirahook to aliases

--- aliases.zsh
+++ aliases.zsh
@@ -13,3 +13,47 @@ gm() {
     git checkout main
   fi
 }
+
+# auto add jira tag to git commits
+gitsetupjirahook() {
+  write_hook() {
+    HOOKS_PATH=$1
+    GIT_DIR_PATH=$PWD/.git
+
+    # .git/hooks or .git/modules/<submodule>/hooks
+    mkdir -p $HOOKS_PATH
+
+    # Write git hook
+    cat <<EOF > $HOOKS_PATH/prepare-commit-msg
+#!/bin/bash
+
+# get current branch
+branchName=\`GIT_DIR=$GIT_DIR_PATH git rev-parse --abbrev-ref HEAD\`
+
+# search jira issue id in a pattern such a "[feature]/ABC-123-description"
+jiraId=\$(echo \$branchName | sed -nr 's,[a-z/]*([A-Z]+-[0-9]+)-.+,\1,p')
+
+# only prepare commit message if pattern matched and jiraId was found
+if [[ ! -z \$jiraId ]]; then
+ # \$1 is the name of the file containing the commit message
+ sed -i.bak -e "1s/^/\$jiraId: /" \$1
+ echo Jira \$jiraId added to commit message
+fi
+EOF
+
+    # Make hook executable
+    chmod +x $HOOKS_PATH/prepare-commit-msg
+    echo Hook $HOOKS_PATH/prepare-commit-msg added
+  }
+
+  # Add hook to root git
+  write_hook .git/hooks
+
+  # Add hook to submodules
+  for submodule in .git/modules/* ; do
+    if [[ -d "$submodule" ]]; then
+      write_hook "$submodule/hooks"
+    fi
+  done
+}
+
